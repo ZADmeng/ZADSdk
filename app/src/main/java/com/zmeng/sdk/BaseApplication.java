b3d408d0d8242ef84316ed5b95766b53f8ff4ced
wangdongyi <wangdongyi@zmeng123.com>
2018-09-20 11:57:26 +0800

    13

---
+++ app/src/main/java/com/zmeng/sdk/BaseApplication.java
@@ -0,0 +1,17 @@
+package com.zmeng.sdk;
+
+import android.app.Application;
+
+import com.zad.sdk.main.ZADSdk;
+
+/**
+ * 作者：王东一
+ * 创建时间：2018/9/20.
+ */
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ZADSdk.init(this,"aaa","ddd");
+    }
+}
