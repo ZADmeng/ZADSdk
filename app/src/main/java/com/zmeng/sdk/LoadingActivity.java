3bb424d3fadd7ff3ae8461a2a96403bb0391bdfd
wangdongyi <wangdongyi@zmeng123.com>
2018-10-12 17:26:08 +0800

    17

---
+++ app/src/main/java/com/zmeng/sdk/LoadingActivity.java
@@ -0,0 +1,88 @@
+package com.zmeng.sdk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+import com.zad.sdk.model.AdError;
+import com.zad.sdk.splash.ZADSplash;
+import com.zad.sdk.splash.ZADSplashListener;
+
+/**
+ * 作者：王东一
+ * 创建时间：2018/9/21.
+ */
+public class LoadingActivity extends AppCompatActivity {
+    private LinearLayout main;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case 2001:
+                    init();
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        initView();
+    }
+
+    private void initView() {
+        main = (LinearLayout) findViewById(R.id.main);
+        handler.sendEmptyMessage(2001);
+    }
+
+    private void init() {
+
+        new ZADSplash(this, main, "aaa", new ZADSplashListener() {
+            @Override
+            public void onNoAD(AdError error) {
+                Log.e("错误" + error.getErrorCode(), error.getErrorMsg());
+//                main.post(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        handler.sendEmptyMessage(2001);
+//                    }
+//                });
+//                if(error.getErrorCode()==2001){
+//                    Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                }
+            }
+
+            @Override
+            public void onADDismissed() {
+                Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onADPresent() {
+
+            }
+
+            @Override
+            public void onADClicked() {
+
+            }
+
+            @Override
+            public void onADExposure() {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/zmeng/sdk/MainActivity.java
+++ app/src/main/java/com/zmeng/sdk/MainActivity.java
@@ -1,55 +1,175 @@
 package com.zmeng.sdk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import com.zad.sdk.banner.ZADBanner;
+import com.zad.sdk.banner.ZADBannerListener;
+import com.zad.sdk.interstitial.ZADInterstitial;
+import com.zad.sdk.interstitial.ZADInterstitialListener;
 import com.zad.sdk.model.AdError;
-import com.zad.sdk.splash.ZADSplash;
-import com.zad.sdk.splash.ZADSplashListener;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout main;
+    private TextView text;
+    private FrameLayout bannerContainer;
+    private TextView text1;
+    private TextView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        init();
     }
 
     private void initView() {
-        main = (LinearLayout) findViewById(R.id.main);
-        new ZADSplash(this, main, "aaa", new ZADSplashListener() {
+        text = (TextView) findViewById(R.id.text);
+        bannerContainer = (FrameLayout) findViewById(R.id.bannerContainer);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getIAD().setZADListener(new ZADInterstitialListener() {
+                    @Override
+                    public void onNoAD(AdError adError) {
+
+                    }
+
+                    @Override
+                    public void onADReceive() {
+                        zadInterstitial.showAsPopupWindow();
+                    }
+
+                    @Override
+                    public void onADOpened() {
+
+                    }
+
+                    @Override
+                    public void onADExposure() {
+
+                    }
+
+                    @Override
+                    public void onADClicked() {
+
+                    }
+
+                    @Override
+                    public void onADClosed() {
+
+                    }
+
+                    @Override
+                    public void onADLeftApplication() {
+
+                    }
+                });
+                zadInterstitial.loadAD();
+            }
+        });
+
+        text1 = (TextView) findViewById(R.id.text1);
+        back = (TextView) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,LoadingActivity.class);
+                startActivity(intent);
+                onBackPressed();
+            }
+        });
+        text1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getIAD().setZADListener(new ZADInterstitialListener() {
+                    @Override
+                    public void onNoAD(AdError adError) {
+
+                    }
+
+                    @Override
+                    public void onADReceive() {
+                        zadInterstitial.show();
+                    }
+
+                    @Override
+                    public void onADOpened() {
+
+                    }
+
+                    @Override
+                    public void onADExposure() {
+
+                    }
+
+                    @Override
+                    public void onADClicked() {
+
+                    }
+
+                    @Override
+                    public void onADClosed() {
+
+                    }
+
+                    @Override
+                    public void onADLeftApplication() {
+
+                    }
+                });
+                zadInterstitial.loadAD();
+            }
+        });
+    }
+
+    private void init() {
+        ZADBanner zadBanner = new ZADBanner(this, "bb");
+        zadBanner.setZADListener(new ZADBannerListener() {
             @Override
             public void onNoAD(AdError error) {
-                Log.e("错误"+error.getErrorCode(), error.getErrorMsg());
-            }
-
-            @Override
-            public void onADDismissed() {
-//                Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
-//                startActivity(intent);
-//                finish();
-            }
-
-            @Override
-            public void onADPresent() {
-
-            }
-
-            @Override
-            public void onADClicked() {
-
+                Log.e("zoubuzou ",error.getErrorMsg());
             }
 
             @Override
             public void onADExposure() {
+                Log.e("zoubuzou ","sdas");
+            }
 
+            @Override
+            public void onADClicked() {
+                Log.e("zoubuzou ","sdadfff");
             }
         });
+        bannerContainer.addView(zadBanner);
+        zadBanner.loadAD();
+
+    }
+
+    ZADInterstitial zadInterstitial;
+
+    private ZADInterstitial getIAD() {
+        if (this.zadInterstitial != null) {
+            zadInterstitial.closePopupWindow();
+            zadInterstitial = null;
+        }
+        zadInterstitial = new ZADInterstitial(this, "ccc");
+        return zadInterstitial;
+    }
+
+
+
+
+    private void closeAsPopup() {
+        if (zadInterstitial != null) {
+            zadInterstitial.closePopupWindow();
+        }
     }
 }
